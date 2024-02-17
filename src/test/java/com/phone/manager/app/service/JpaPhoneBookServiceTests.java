8665a730dfbd3584e38f079fb845c6897926a99d
paul <bares.paul@gmail.com>
2024-02-17 18:30:25 +0400

    fix tests

--- src/test/java/com/phone/manager/app/service/JpaPhoneBookServiceTests.java
+++ src/test/java/com/phone/manager/app/service/JpaPhoneBookServiceTests.java
@@ -2,16 +2,13 @@ package com.phone.manager.app.service;
 
 import com.phone.manager.app.repository.JpaPhoneRepository;
 import com.phone.manager.app.repository.PhoneRepository;
-import com.phone.manager.app.spring.conf.PhoneBookingServiceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 
 /**
  * Test class for {@link PhoneBookingService} using a {@link JpaPhoneRepository}.
  */
 @DataJpaTest
-@Import(PhoneBookingServiceConfiguration.class)
 class JpaPhoneBookServiceTests extends APhoneBookServiceTests {
 
   @Autowired
--- src/test/java/com/phone/manager/app/service/SpringPhoneBookServiceIntegrationTests.java
+++ src/test/java/com/phone/manager/app/service/SpringPhoneBookServiceIntegrationTests.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +24,6 @@ import java.util.concurrent.atomic.AtomicReference;
 import static com.phone.manager.app.Constants.PHONE_NAMES;
 
 @DataJpaTest
-@Transactional
 @Import(PhoneBookingServiceConfiguration.class)
 class SpringPhoneBookServiceIntegrationTests {
 
