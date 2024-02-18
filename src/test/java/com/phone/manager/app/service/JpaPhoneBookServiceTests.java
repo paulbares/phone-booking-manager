bb3623f01f41b85f187635df1824d68eac9e92a9
paul <bares.paul@gmail.com>
2024-02-18 14:30:45 +0400

    more tests controller

--- src/test/java/com/phone/manager/app/service/JpaPhoneBookServiceTests.java
+++ src/test/java/com/phone/manager/app/service/JpaPhoneBookServiceTests.java
@@ -1,31 +1,33 @@
 package com.phone.manager.app.service;
 
 import com.phone.manager.app.repository.JpaPhoneRepository;
-import com.phone.manager.app.spring.config.PhoneBookingServiceConfig;
+import com.phone.manager.app.spring.SpringPhoneBookingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Primary;
-
-import java.time.Instant;
-import java.util.function.Supplier;
 
 /**
  * Test class for {@link PhoneBookingService} using a {@link JpaPhoneRepository}.
  */
 @DataJpaTest
-@Import({PhoneBookingServiceConfig.class, JpaPhoneBookServiceTests.JpaPhoneBookServiceTestConfiguration.class})
+@Import(JpaPhoneBookServiceTests.JpaPhoneBookServiceTestConfiguration.class)
 class JpaPhoneBookServiceTests extends APhoneBookServiceTests {
 
   @TestConfiguration
   static class JpaPhoneBookServiceTestConfiguration {
+
     @Bean
-    @Primary
-    public Supplier<Instant> timeSupplierForTest() {
-      return () -> currentTime;
+    public PhoneBookingService phoneBookingService(JpaPhoneRepository repository) {
+      return new SpringPhoneBookingService(new PhoneBookingServiceImpl(repository, () -> currentTime));
     }
+
+//    @Bean
+//    @Primary
+//    public Supplier<Instant> timeSupplierForTest() {
+//      return () -> currentTime;
+//    }
   }
 
   @Autowired
